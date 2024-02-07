1a3204ff74b29cb79873b07810a0991810ba0b5a
milos pusic <milos.pusic@prvabeogim.edu.rs>
2024-02-07 20:06:51 +0100

    ver 0.2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Poligon2024
     {
         static void Main(string[] args)
         {
+            /*
             tacka A= new tacka(0,4);
             tacka B= new tacka(1,3);
             tacka C= new tacka(3,2);
@@ -18,6 +19,13 @@ namespace Poligon2024
             vektor prvi = new vektor(A, B);
             vektor drugi = new vektor(C, D);
             Console.WriteLine("AB * CD = {0}", vektor.skalarni(prvi, drugi));
+            */
+            Console.WriteLine("Pritisni:");
+            Console.WriteLine("1: Unos");
+            Console.WriteLine("2: SAVE");
+            Console.WriteLine("3: LOAD");
+            Console.WriteLine("0: EXIT");
+            int izbor = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
--- poligon.cs
+++ poligon.cs
@@ -8,5 +8,17 @@ namespace Poligon2024
 {
     internal class poligon
     {
+        int broj_temena;
+        tacka[] teme;
+        public poligon() 
+        { 
+            teme = new tacka[broj_temena];
+        }
+        public poligon(int n)
+        {
+            broj_temena= n;
+            teme = new tacka[broj_temena];
+        }
+
     }
 }
